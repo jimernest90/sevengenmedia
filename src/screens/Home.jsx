9229a5eb0c6c66ea37aba37a69e4f0b7dd1bf62a
james white jr <jameswhitejr.@jamess-mbp-2.myfiosgateway.com>
2020-11-22 00:59:45 -0500

    fixed some issues

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -9,6 +9,13 @@ const Home = () =>{
         <div className='box'>
             <h2>Movies, Documentaries, Screenwriting…On The Rise!</h2>
             <img src={logo} alt='logo' class='logo'/>
+            <div className='awards'>
+                <div className='award'>2020 - Community Service Award/ Women in Media</div>
+                <div className='award'>2015 - Director of the Year Award/ Princeton TV</div>
+                <div className='award'>2014 - Historical Documentary Award/ New Brunswick African American Cultural and Heritage Committee (NJ)
+                </div>
+                <div className='award'>2006- Broader Vision Award/ Garden State Film Festival</div>
+            </div>
         </div>
         </div>
     )
